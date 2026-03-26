c3d03e72e0ed4119bcc0e6aa27bdac2c5be0ceca
WellC2G <fedorka2010@gmail.com>
2026-03-26 14:46:44 +0300

    Add cache for temp .vtm file

--- gui/parse_core/get_data/get_element_geometry.py
+++ gui/parse_core/get_data/get_element_geometry.py
@@ -9,14 +9,29 @@ from pathlib import Path
 
 def get_element_geometry(model: ifcopenshell.file) -> dict:
     try:
+        temp_dir = Path(tempfile.gettempdir())
+
+        projects = model.by_type("IfcProject")
+        project_id = projects[0].GlobalId if projects else "unknown_project"
+
+        file_path = temp_dir / f"ifc_model_{project_id}.vtm"
+
+        if file_path.exists():
+            print(f"Cache found! Fast loading from: {file_path}")
+            cached_model = pv.read(str(file_path))
+            return {
+                "file_path": str(file_path),
+                "elements_count": cached_model.n_blocks
+            }
+
+        print("Cache not found. Starting geometry generation...")
+
         settings = ifcopenshell.geom.settings()
         settings.set("use-world-coords", True)
         settings.set("boolean-attempt-2d", True)
         settings.set("weld-vertices", True)
         settings.set("mesher-linear-deflection", 0.3)
         settings.set("mesher-angular-deflection", 0.5)
-        #Для Brep геометрии
-        #settings.set("iterator-output", ifcopenshell.ifcopenshell_wrapper.SERIALIZED)
 
         num_cores = multiprocessing.cpu_count()
         iterator = ifcopenshell.geom.iterator(settings, model, num_cores)
@@ -39,16 +54,13 @@ def get_element_geometry(model: ifcopenshell.file) -> dict:
             faces_pv = faces_pv.flatten()
 
             mesh = pv.PolyData(verts_np, faces_pv)
-
             multiblock[global_id] = mesh
 
             if not iterator.next():
                 break
 
-        temp_dir = Path(tempfile.gettempdir())
-        file_path = temp_dir / "full_ifc_model.vtm"
-
         multiblock.save(str(file_path))
+        print("Geometry successfully generated and saved to cache.")
 
         return {
             "file_path": str(file_path),
